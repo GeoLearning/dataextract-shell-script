2040a802e10776ab8fe21c5ed3ecf4917ef86c3f
bcarlso <bcarlso@gmail.com>
2009-07-20 09:49:14 -0500

    Updated AWK script

--- request_class_list.sh
+++ request_class_list.sh
@@ -1,2 +1,2 @@
 # Requests a class list based on dates specified as args
-curl -u lmsusername:S3cr3t! -d "header_required=1&fromdate=$1&todate=$2" --dump-header /dev/stdout --output /dev/null --silent https://services.geolearning.com/dataextraction/your-domain/classes | grep Location | awk '{ print "--url", $2 }' | curl -u lmsusername:S3cr3t! --config - > ~/class-list.csv
+curl -u lmsusername:S3cr3t! -d "header_required=1&fromdate=$1&todate=$2" --dump-header /dev/stdout --output /dev/null --silent https://services.geolearning.com/dataextraction/your-domain/classes | awk '/Location/ { print "--url", $2 }' | curl -u lmsusername:S3cr3t! --config - > ~/class-list.csv
